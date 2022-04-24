f66aa1e3a5eceda5389de4bb2223de5c85cbc41b
achmad.widyanto <achmad.widyanto@spesolution.com>
2022-04-24 10:05:44 +0700

    change float32 to float64

--- graphql/schema/model/toko.go
+++ graphql/schema/model/toko.go
@@ -3,7 +3,7 @@ package model
 type(
 	 Product struct{
 		ID		string  `json:"id"`
-		Price		float32 `json:"price"`
+		Price		float64 `json:"price"`
 		ProductName	string  `json:"product_name"`
 		QteStock	int     `json:"qte_stock"`
 		ProductGambar	string  `json:"product_gambar"`	
--- graphql/schema/mutation/resolvermutation.go
+++ graphql/schema/mutation/resolvermutation.go
@@ -8,7 +8,7 @@ import (
 
 func CreateProdcutMutation(param graphql.ResolveParams) (interface{}, error) {
 	id := param.Args["id"].(string)
-	price := param.Args["price"].(float32)
+	price := param.Args["price"].(float64)
 	productname := param.Args["product_name"].(string)
 	qty := param.Args["qte_stock"].(int)
 	productgambar := param.Args["product_gambar"].(string)
@@ -17,7 +17,7 @@ func CreateProdcutMutation(param graphql.ResolveParams) (interface{}, error) {
 	if err != nil {
 		panic(err.Error())
 	}
-	_, err = db.Query("INSERT INTO toko.product values (?,?,?,?,?)", id, price, productname, qty, productgambar)
+	_, err = db.Query("INSERT INTO toko.products values (?,?,?,?,?)", id, price, productname, qty, productgambar)
 	if err != nil {
 		panic(err.Error())
 	}
