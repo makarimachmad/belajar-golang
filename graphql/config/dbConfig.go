901a801a46162ce8e981b43af356a3d78f52ef64
achmad.widyanto <achmad.widyanto@spesolution.com>
2022-04-24 09:58:07 +0700

    add package mysql

--- graphql/config/dbConfig.go
+++ graphql/config/dbConfig.go
@@ -1,6 +1,7 @@
 package config
 
 import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
 
 func GetConnection() (*sql.DB, error){
 	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/toko")
